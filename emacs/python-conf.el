1860844148a34d5fa1892db06426613224cbf27a
Kenshi Kawaguchi <kenshi@recurse.ca>
2012-05-08 12:57:30 -0700

    Add nicer auto-indentation of multi-line statements

--- emacs/python-conf.el
+++ emacs/python-conf.el
@@ -5,3 +5,25 @@
      (define-key python-mode-map "\C-m" 'newline-and-indent)))
 
 (setq-default py-indent-offset 4)
+
+;; Customize indentation
+;; http://stackoverflow.com/questions/4293074/in-emacs-python-mode-customize-multi-line-statement-indentation
+
+(defadvice python-calculate-indentation (around outdent-closing-brackets)
+  "Handle lines beginning with a closing bracket and indent them so that
+they line up with the line containing the corresponding opening bracket."
+  (save-excursion
+    (beginning-of-line)
+    (let ((syntax (syntax-ppss)))
+      (if (and (not (eq 'string (syntax-ppss-context syntax)))
+               (python-continuation-line-p)
+               (cadr syntax)
+               (skip-syntax-forward "-")
+               (looking-at "\\s)"))
+          (progn
+            (forward-char 1)
+            (ignore-errors (backward-sexp))
+            (setq ad-return-value (current-indentation)))
+        ad-do-it))))
+
+(ad-activate 'python-calculate-indentation)
