6fa3a22d0a0bdcfe7bd2e9e0ab21ccc3bdc5b762
Kenshi Kawaguchi <kenshi@recurse.ca>
2013-04-24 16:18:19 -0700

    some emacs updates

--- emacs/js2-conf.el
+++ emacs/js2-conf.el
@@ -3,6 +3,8 @@
 (autoload 'js2-mode "js2-mode" nil t)
 (add-to-list 'auto-mode-alist '("\\.js$" . js2-mode))
 
+(setq js2-highlight-level 1)
+
 ;; javascript
 (defun my-js2-indent-function ()
   (interactive)
