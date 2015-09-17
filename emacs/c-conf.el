f461b5ccc7e4bd80ac9bdb41d997b1f9f6154135
Kenshi Kawaguchi <kenshi@recurse.ca>
2015-09-17 11:50:25 -0700

    Ugh... C. Indentation.

--- emacs/c-conf.el
+++ emacs/c-conf.el
@@ -1,6 +1,9 @@
 ;; See http://emacswiki.org/emacs/IndentingC#toc4
 ;; Still trying to figure this one out...
+;; http://www.emacswiki.org/emacs/IndentationBasics
 
-(setq-default c-basic-offset 4
-              tab-width 4
+(setq-default c-basic-offset 2
+              tab-width 2
               indent-tabs-mode t)
+(setq c-basic-offset 2)
+(c-set-offset 'case-label '+)
