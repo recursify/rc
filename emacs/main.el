4dcb0a38c71209a7fee42738bf22e7f368db85eb
Kenshi Kawaguchi <kenshi@recurse.ca>
2021-12-15 13:46:34 -0800

    Prune main emacs files

--- emacs/main.el
+++ emacs/main.el
@@ -1,22 +1,10 @@
-(when (>= emacs-major-version 24)
-  (require 'package)
-  (package-initialize)
-  (add-to-list 'package-archives '("melpa" . "http://melpa.milkbox.net/packages/") t)
-  )
-
-;; Allow mouse scrolling from terminal
-;;(xterm-mouse-mode)
-;;(global-set-key (kbd "<mouse-4>") 'scroll-down-line)
-;;(global-set-key (kbd "<mouse-5>") 'scroll-up-line)
-
+(require 'package)
+(add-to-list 'package-archives '("melpa" . "https://melpa.org/packages/") t)
 
 ;;God, this is annoying
 ;; http://stackoverflow.com/questions/29328960/
 ;;       how-to-disable-auto-indentation-on-newline-in-js2-mode
 (electric-indent-mode -1)
-
-;;(autoload 'espresso-mode "espresso")
-;;(load "highlight-chars")
 
 (setq-default indent-tabs-mode nil)
 (setq-default tab-width 2)
@@ -24,11 +12,6 @@
 (setq mac-command-modifier 'meta)
 
 (add-hook 'before-save-hook 'delete-trailing-whitespace)
-(autoload 'php-mode "php-mode" "PHP mode" t)
-
-(setq auto-mode-alist (append '(("\\.php$" . php-mode))
-                              auto-mode-alist))
-
 (setq next-screen-context-lines 6)
 
 
@@ -38,7 +21,7 @@
 (load "~/rc/emacs/js2-conf-basic.el")
 (load "~/rc/emacs/python-conf.el")
 (load "~/rc/emacs/ruby-conf.el")
-(load "~/rc/emacs/yaml-conf.el")
+;;(load "~/rc/emacs/yaml-conf.el")
 (load "~/rc/emacs/go-conf.el")
 (load "~/rc/emacs/c-conf.el")
 (load "~/rc/emacs/proto-conf.el")
@@ -48,6 +31,9 @@
 (load "~/rc/emacs/projectile-conf.el")
 (load "~/rc/emacs/tide-conf.el")
 ;; (load "~/rc/emacs/clipboard.el")
+
+;; dumb jump
+(add-hook 'xref-backend-functions #'dumb-jump-xref-activate)
 
 (defface egoge-display-time
   '((((type x w32 mac))
@@ -74,8 +60,7 @@
  '(js2-basic-offset 2)
  '(js2-bounce-indent-p t)
  '(package-selected-packages
-   (quote
-    (helm-ack haml-mode go-mode pug-mode company-c-headers tide flycheck company jinja2-mode yaml-mode typescript-mode markdown-mode js2-mode helm-projectile haskell-mode column-marker coffee-mode ack))))
+   '(helm-ag helm-rg dumb-jump helm-ack haml-mode go-mode pug-mode company-c-headers tide flycheck company jinja2-mode yaml-mode typescript-mode markdown-mode js2-mode helm-projectile haskell-mode column-marker coffee-mode ack)))
 (custom-set-faces
  ;; custom-set-faces was added by Custom.
  ;; If you edit it by hand, you could mess it up, so be careful.
